8585ad44ec6b341e9e9eb10819f104c461e347a7
gowhale <gabewhale@gmail.com>
2020-12-02 20:05:05 +0000

    main file created, bare bones implemented

---
+++ main.py
@@ -0,0 +1,58 @@
+import csv
+from time import sleep
+from random import randrange
+import datetime
+
+TIME_BETWEEN_READINGS = 3  # Seconds between readings
+
+
+def get_temprature():
+    # TODO: get tempreture from rasbperry pi sensor rather than just random number
+    return (randrange(10))
+
+
+def get_humidity():
+    # TODO: get humidity from rasbperry pi sensor rather than just random number
+    return (randrange(10))
+
+
+def pretty_time(raw_time):
+    return (raw_time.strftime("%H:%M:%S"))
+
+def pretty_date(raw_time):
+    return (raw_time.strftime("%Y:%m:%d"))
+
+def get_current_time_object():
+    currentDT = datetime.datetime.now()
+
+    return currentDT
+
+
+def create_csv():
+    # TODO : Create a new CSV with unique name
+    headers = [["time","temprature","humidity"]]
+    current_datetime = get_current_time_object()
+    # file_name = pretty_time(current_datetime) + "-" + pretty_date(current_datetime) + ".csv"
+    file_name = ("{}-{}.csv").format(pretty_time(current_datetime), pretty_date(current_datetime))
+    with open(file_name, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(headers)
+
+
+def main():
+    # TODO : capture enviroment temp and visualise data
+    create_csv()
+
+    for _ in range(5):
+        temprature = get_temprature()
+        humidity = get_humidity()
+        current_time = get_current_time_object()
+        row = [pretty_time(current_time), temprature, humidity]
+        print(row)
+
+        sleep(TIME_BETWEEN_READINGS)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
