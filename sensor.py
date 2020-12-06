a0ed3ba3774fd62864acc019e5310d40249a9e15
gowhale <gabewhale@gmail.com>
2020-12-06 22:25:36 +0000

    Added pressure to log and refactored sensor to class

---
+++ sensor.py
@@ -0,0 +1,28 @@
+from bme280 import *
+
+
+class Sensor ():
+
+    def __init__(self):
+        (chip_id, chip_version) = readBME280ID()
+        print("Chip ID     :", chip_id)
+        self.chip_id = chip_id
+        self.chip_version = chip_version
+
+        temperature, pressure, humidity = readBME280All()
+
+        self.temperature = temperature
+        self.pressure = pressure
+        self.humidity = humidity
+
+    def get_temperature(self):
+        temperature, _, _ = readBME280All()
+        return temperature
+
+    def get_humidity(self):
+        _, humidity, _ = readBME280All()
+        return humidity
+
+    def get_pressure(self):
+        _, _, pressure = readBME280All()
+        return pressure
