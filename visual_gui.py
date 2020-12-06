6baafd6836c5574bfc4dde0b60b01608e536048e
gowhale <gabewhale@gmail.com>
2020-12-06 14:47:47 +0000

    Now takes a reading at the start of every minute, plus added comments

--- visual_gui.py
+++ visual_gui.py
@@ -5,6 +5,7 @@ import time
 from random import randrange
 from colours import ColourScale
 from weather_api import get_weather
+import datetime
 
 # def draw_scale():
 
@@ -14,15 +15,24 @@ def text_objects(text, font):
     return text_surface, text_surface.get_rect()
 
 
+def get_current_ss():
+    return (datetime.datetime.now().strftime("%S"))
+
+
+def get_current_hh():
+    return (datetime.datetime.now().strftime("%M"))
+
+
 def main():
     pygame.init()
 
+    DISPLAY = pygame.display.set_mode((500, 400), 0, 32)
+    pygame.display.set_caption("temperature Analysis") 
+    WHITE = (255, 255, 255)
+    DISPLAY.fill(WHITE)
+
     house_top_corner = {"x": 200, "y": 150}
     house_dimensions = {"height": 100, "width": 100}
-
-    DISPLAY = pygame.display.set_mode((500, 400), 0, 32)
-
-    WHITE = (255, 255, 255)
 
     colour_scale = ColourScale("blue", "red", 100)
     colour_scale.set_max_temp(30)
@@ -30,13 +40,11 @@ def main():
 
     print(colour_scale.get_colours())
 
-    DISPLAY.fill(WHITE)
-
     scale_locations = {"x": 50, "y": 50}
     scale_dimensions = {"height": 10, "width": 10}
 
     for part in range(1, 31):
-        current_colour = colour_scale.get_tempreture_colours_pigame_format(
+        current_colour = colour_scale.get_temperature_colours_pigame_format(
             part)
 
         scale_coordinates = (
@@ -53,41 +61,51 @@ def main():
         DISPLAY.blit(text_surface, text_rect)
 
     temp = 1
+    pygame.display.update()
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        temp = (temp + 0.5) % 30
-        print("TEMP IS: {}".format(temp))
-        print(temp)
-        pygame.display.update()
-        square_coordinates = (
-            house_top_corner["x"], house_top_corner["y"], house_dimensions["width"], house_dimensions["height"])
 
-        current_colour = colour_scale.get_tempreture_colours(temp)
-        interpolated_colour = (int(current_colour.get_red(
-        )*255), int(current_colour.get_green()*255), int(current_colour.get_blue()*255))
-        pygame.draw.rect(
-            DISPLAY, interpolated_colour, square_coordinates)
-        polygon_coordinates = [[house_top_corner["x"], house_top_corner["y"]], [house_top_corner["x"] + int(house_dimensions["width"]/2), house_top_corner["y"] - int(
-            house_dimensions["height"]/2)], [house_top_corner["x"] + house_dimensions["width"], house_top_corner["y"]]]
-        print(interpolated_colour)
-        pygame.draw.polygon(DISPLAY, interpolated_colour,
-                            polygon_coordinates, 0)
+        if(get_current_ss() == "00"):
+            # if(int(get_current_hh()) % 5 == 00):
 
-        circle_location = {"x": int(house_top_corner["x"] + house_dimensions["width"]/2), "y": int(
-            house_top_corner["y"] + house_dimensions["height"]/3)}
+            temp = (temp + 0.5) % 30
+            print("TEMP IS: {}".format(temp))
+            print(temp)
 
-        enviroment_tempreture = (float(get_weather("Cardiff")))
-        print("ENVIROMENT TEMP: {}".format(enviroment_tempreture))
-        enviroment_colour = colour_scale.get_tempreture_colours_pigame_format(
-            enviroment_tempreture)
+            pygame.display.update()
 
-        pygame.draw.circle(DISPLAY, enviroment_colour, (circle_location["x"], circle_location["y"]), int(
-            house_dimensions["height"]*1.5), 10)
+            # Creates the square part of the house
+            square_coordinates = (
+                house_top_corner["x"], house_top_corner["y"], house_dimensions["width"], house_dimensions["height"])
+            current_colour = colour_scale.get_temperature_colours_pigame_format(
+                temp)
+            pygame.draw.rect(
+                DISPLAY, current_colour, square_coordinates)
 
-        time.sleep(5)
+            # Creates the top of the house
+            polygon_coordinates = [[house_top_corner["x"], house_top_corner["y"]], [house_top_corner["x"] + int(house_dimensions["width"]/2), house_top_corner["y"] - int(
+                house_dimensions["height"]/2)], [house_top_corner["x"] + house_dimensions["width"], house_top_corner["y"]]]
+            print(current_colour)
+            pygame.draw.polygon(DISPLAY, current_colour,
+                                polygon_coordinates, 0)
+
+            # Creates the circle around the house simulating weather
+            circle_location = {"x": int(house_top_corner["x"] + house_dimensions["width"]/2), "y": int(
+                house_top_corner["y"] + house_dimensions["height"]/3)}
+            enviroment_temperature = (float(get_weather("Cardiff")))
+            print("ENVIROMENT TEMP: {}".format(enviroment_temperature))
+
+            enviroment_colour = colour_scale.get_temperature_colours_pigame_format(
+                enviroment_temperature)
+
+            pygame.draw.circle(DISPLAY, enviroment_colour, (circle_location["x"], circle_location["y"]), int(
+                house_dimensions["height"]*1.5), 10)
+
+            # Waits for 1 second so that the API is only called once
+            time.sleep(1)
 
 
 main()
